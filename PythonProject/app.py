168d806ca13b1429bc9eec66242f9a3ceea67a62
Alexander Bobyliev <neverdies.cc@gmail.com>
2025-04-10 09:57:08 +0200

    first commit

---
+++ PythonProject/app.py
@@ -0,0 +1,44 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+main = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text='Позивачам'), KeyboardButton(text='Інвесторам')],
+        [KeyboardButton(text='Адвокатам'), KeyboardButton(text='Наша команда')],
+        [KeyboardButton(text='Бажаєте стати нашим представником?'), KeyboardButton(text='Про нас')],
+        [KeyboardButton(text='Відправити запит')],
+        [KeyboardButton(text='Контакти')]
+    ]
+)
+
+pozer = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text='Позивачам/Відповідачам')],
+        [KeyboardButton(text='Позови, що фінансуються')],
+        [KeyboardButton(text='Відправити запит')],
+        [KeyboardButton(text='Назад')]
+    ]
+)
+
+
+
+money = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text='Позивачам/Відповідачам')],
+        [KeyboardButton(text='Позови, що фінансуються')],
+        [KeyboardButton(text='Міжнародний досвід інвестицій в літігацію')],
+        [KeyboardButton(text='Принципи інвестицій в позови')],  # Додана кнопка
+        [KeyboardButton(text='Зробити дзвінок')],
+        [KeyboardButton(text='Назад')]  # Додана кнопка
+    ]
+)
+
+
+lawyer = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text='Адвокатам/Інфо')],
+                                      [KeyboardButton(text='Позови, що фінансуються')],
+                                      [KeyboardButton(text='Назад')]])
+
+
+
+
+
+
